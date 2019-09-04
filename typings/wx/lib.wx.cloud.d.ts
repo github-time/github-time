7b380310289198e597194109d467d1ca6d5c13f2
vaniship <vaniship@gmail.com>
2019-09-04 15:17:46 +0800

    fix: 修正 wx.cloud 的类型文件定义

--- typings/wx/lib.wx.cloud.d.ts
+++ typings/wx/lib.wx.cloud.d.ts
@@ -1,5 +1,5 @@
 /*! *****************************************************************************
-Copyright (c) 2018 Tencent, Inc. All rights reserved. 
+Copyright (c) 2018 Tencent, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -117,7 +117,7 @@ declare namespace WXNS {
   interface uploadFile {
     /**
      * upload file
-     * @param param 
+     * @param param
      */
     (param: IUploadFileParam): IUploadFileTask
   }
@@ -142,7 +142,7 @@ declare namespace WXNS {
   interface downloadFile {
     /**
      * download file
-     * @param param 
+     * @param param
      */
     (param: IDownloadFileParam): IDownloadFileTask
   }
@@ -202,7 +202,7 @@ declare namespace WXNS {
 }
 
 declare namespace wx {
-  interface WX {
+  interface Wx {
     cloud: {
       init: (config?: ICloudConfig) => void,
 
@@ -667,7 +667,7 @@ declare namespace DB {
   export interface ICountResult extends IAPISuccessParam {
     total: number
   }
-  
+
 }
 
 /**
