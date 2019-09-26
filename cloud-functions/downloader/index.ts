1aa3358be219c428261134063b75dfde70223753
vaniship <vaniship@gmail.com>
2019-09-26 17:42:46 +0800

    fix: 修正下载代理云函数

--- cloud-functions/downloader/index.ts
+++ cloud-functions/downloader/index.ts
@@ -6,7 +6,6 @@ const MAX_FILE_SIZE = 3 * 1024 * 1024 // 限制最大为4M
 const cacheDir = 'downloader-cache'
 const baseFileId = `github-time-mp.6769-github-time-mp-1300157824/${cacheDir}`
 
-const SPEED = 1000 / 1024 / 1024 / 0.04 // 每秒 0.04 M
 cloud.init()
 // 云函数入口函数
 export async function main (event: any) {
@@ -19,8 +18,6 @@ export async function main (event: any) {
     fileList: [`cloud://${baseFileId}/${gitHash}`]
   })
 
-  let wait = 0
-
   if (!result.fileList[0].tempFileURL) {
     const res = await axios({
       method: 'get',
@@ -29,21 +26,23 @@ export async function main (event: any) {
     })
 
     const size = res.headers['content-length']
-    wait = Math.floor(size * SPEED)
     if (size > MAX_FILE_SIZE) {
       return {
         status: 413 // 超过文件大小限制
       }
     }
 
-    cloud.uploadFile({
+    await cloud.uploadFile({
       cloudPath: `${cacheDir}/${gitHash}`,
       fileContent: res.data
     })
+
+    return {
+      status: 202 // 服务器已接收到上传请求，正在处理中
+    }
   }
 
   return {
-    status: 200,
-    wait
+    status: 200 // 可直接下载
   }
 }
