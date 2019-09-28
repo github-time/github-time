f6677ad6ca85cd0ca27645b6837d46e7c0822875
vaniship <vaniship@gmail.com>
2019-09-28 08:17:02 +0800

    fix: 增加判空

--- miniprogram/common/components/fill-view/index.ts
+++ miniprogram/common/components/fill-view/index.ts
@@ -10,7 +10,7 @@ Component({
       const query = wx.createSelectorQuery().in(this)
       query.select('.fill-view').boundingClientRect().exec((res: any) => {
         const top = res[0].top + 'px'
-        const tabBarHeight = this.getTabBar() ? '92rpx' : '0px'
+        const tabBarHeight = this.getTabBar && this.getTabBar() ? '92rpx' : '0px'
         this.setData!({
           otherHeight: `(${top} + ${tabBarHeight})`
         })
