684249ed9b875ad966a6c5c8a620e241355c3e50
vaniship <vaniship@gmail.com>
2019-10-01 23:01:46 +0800

    feat: 增加 loading 提示辅助函数

--- miniprogram/utils/common.ts
+++ miniprogram/utils/common.ts
@@ -1,5 +1,19 @@
+
 const sleep = async (time: number) => new Promise(resolve => { setTimeout(resolve, time) })
 
+async function wrapLoading (title: string, task: Promise<void>|(() => Promise<void>)) {
+  wx.showLoading({
+    title
+  })
+  if (task instanceof Promise) {
+    await task
+  } else {
+    await task()
+  }
+  wx.hideLoading()
+}
+
 export {
-  sleep
+  sleep,
+  wrapLoading
 }
